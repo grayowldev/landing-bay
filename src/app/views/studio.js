4c0a62b816698c7e37ebb9fc3e108ea800009e81
kwasinsiah <truegrayowl@gmail.com>
2024-08-27 19:57:30 -0400

    Added Studio component

---
+++ src/app/views/studio.js
@@ -0,0 +1,11 @@
+export const Studio = () => {
+    return (
+            <div className="flex">
+                {/*<div>Top bar</div>*/}
+                <div className="flex-1 p-4 bg-emerald-600">
+                    Main View
+                </div>
+                <div className="w-64 bg-blue-700"> Sidebar </div>
+            </div>
+    )
+}
