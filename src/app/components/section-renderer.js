4ba88b4209f1b9ec3687fba8304ca9744e3eb378
kwasinsiah <truegrayowl@gmail.com>
2024-08-29 14:53:13 -0400

    Added shadcn

--- src/app/components/section-renderer.js
+++ src/app/components/section-renderer.js
@@ -8,19 +8,29 @@ import {Footer} from "@/app/section/Footer";
 import {Pricing} from "@/app/section/Pricing";
 import {Testimonials} from "@/app/section/Testimonials";
 
-export const SectionRenderer = ({data, children}) => {
+export const SectionRenderer = ({data, activeElement, toggleActive, findElement, children}) => {
+
     return (
         <div className="bg-white">
             {children}
-            {data.type == 'hero' && <Hero></Hero>}
-            {data.type == 'benefits' && <Benefits></Benefits>}
-            {data.type == 'blank' && <BlankSection></BlankSection>}
-            {data.type == 'call-to-action' && <CallToAction></CallToAction>}
-            {data.type == 'faq' && <FAQ></FAQ>}
-            {data.type == 'features' && <Features></Features>}
-            {data.type == 'footer' && <Footer></Footer>}
-            {data.type == 'pricing' && <Pricing></Pricing>}
-            {data.type == 'testimonials' && <Testimonials></Testimonials>}
+            {data.type === 'hero' &&
+                <Hero data={data} activeElement={activeElement} toggleActive={toggleActive} findElement={findElement}></Hero>}
+            {data.type === 'benefits' &&
+                <Benefits></Benefits>}
+            {data.type === 'blank' &&
+                <BlankSection></BlankSection>}
+            {data.type === 'call-to-action' &&
+                <CallToAction></CallToAction>}
+            {data.type === 'faq' &&
+                <FAQ></FAQ>}
+            {data.type === 'features' &&
+                <Features></Features>}
+            {data.type === 'footer' &&
+                <Footer></Footer>}
+            {data.type === 'pricing' &&
+                <Pricing></Pricing>}
+            {data.type === 'testimonials' &&
+                <Testimonials></Testimonials>}
         </div>
     )
 }
