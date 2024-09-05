ce1c82001e16d28a222345f2e737eae375387391
kwasinsiah <truegrayowl@gmail.com>
2024-09-05 19:48:01 -0400

    Added a droppable section

--- src/app/components/page-section.js
+++ src/app/components/page-section.js
@@ -1,24 +1,43 @@
-export const PageSection = ({children, onAddSection}) => {
+import {Button} from "@/components/ui/button";
+import {motion} from "framer-motion";
+
+export const PageSection = ({children, onAddSection, addElementOnClick}) => {
+    const onDrop = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        console.log("Dropping element")
+        console.log(event)
+    }
+    const allowDrop = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+    }
     return (
             <div className='relative group'>
                 <div className="relative border-2 border-transparent group-hover:border-sky-700 p-4">
                     {/* Button positioned in the top-right corner of the children */}
-                    <button
+                    <Button
                         className={`absolute top-14 right-28 transform translate-x-1/2 -translate-y-1/2 bg-black 
-                        text-white px-6 py-2 rounded-full hover:bg-gray-800 transition z-10 opacity-0 group-hover:opacity-100`}>
+                        text-white px-6 py-2 rounded-full hover:bg-gray-800 transition z-10 opacity-0 group-hover:opacity-100`}
+                        onClick={addElementOnClick}>
                         Add element
-                    </button>
-                    <div className="relative">
-                        {children}
-                    </div>
+                    </Button>
+                    <motion.div
+                        onDrop={onDrop}
+                        onDragOver={allowDrop}>
+                        <div className="relative">
+                            {children}
+                        </div>
+                    </motion.div>
+
                 </div>
-                <button className="absolute -bottom-3 left-1/2 transform -translate-x-1/2
+                <Button className="absolute -bottom-3 left-1/2 transform -translate-x-1/2
                         translate-y-1/2 bg-sky-700 text-white text-sm px-2 py-1 rounded opacity-0
                         group-hover:opacity-100 transition-opacity z-10"
                         onClick={onAddSection}
                 >
                     Add Section
-                </button>
+                </Button>
             </div>
 
 
