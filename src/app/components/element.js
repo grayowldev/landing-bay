f0e1be2227c176bc1ac97d98e2fc0d273c0faee6
kwasinsiah <truegrayowl@gmail.com>
2024-09-06 12:25:56 -0400

    Element can now take in custom styles

--- src/app/components/element.js
+++ src/app/components/element.js
@@ -1,7 +1,9 @@
 
-export const Element = ({children, isActive, onClick}) => {
+export const Element = ({children, isActive, onClick, styles}) => {
   return (
-      <div onClick={onClick} className={`hover:border-2 hover:border-sky-700 ${isActive ? 'border-2 border-sky-700' : ''}`}>
+      <div onClick={onClick}
+           className={`hover:border-2 hover:border-sky-700 ${isActive ? 'border-2 border-sky-700' : ''}`}
+           style={styles}>
         {children}
       </div>
   )
