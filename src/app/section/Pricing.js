c75e965c0bcdb3a2e3b5befc0c3d5312ff5d1ecb
kwasinsiah <truegrayowl@gmail.com>
2024-09-07 22:26:29 -0400

    Updated Pricing section with temporary content

--- src/app/section/Pricing.js
+++ src/app/section/Pricing.js
@@ -1,40 +1,133 @@
+import {Render} from "@/app/components/render";
+
 export const Pricing = ({data}) => {
+    const content = [
+        {
+            content: "Pricing"
+        },
+        {
+            type: "container",
+            elements: [
+                {
+                    content: "Starter"
+                },
+                {
+                    content: "$3.99/month"
+                },
+                {
+                    content: "sub feature 1"
+                },
+                {
+                    content: "sub feature 1"
+                },
+                {
+                    content: "sub feature 1"
+                },
+            ]
+        },
+        {
+            type: "container",
+            elements: [
+                {
+                    content: "Plus"
+                },
+                {
+                    content: "$9.99/month"
+                },
+                {
+                    content: "sub feature 1"
+                },
+                {
+                    content: "sub feature 1"
+                },
+                {
+                    content: "sub feature 1"
+                },
+            ]
+        },
+        {
+            type: "container",
+            elements: [
+                {
+                    content: "Pro"
+                },
+                {
+                    content: "$12.99/month"
+                },
+                {
+                    content: "sub feature 1"
+                },
+                {
+                    content: "sub feature 1"
+                },
+                {
+                    content: "sub feature 1"
+                },
+            ]
+        },
+    ]
     return (
         <section className="bg-white py-20">
             <div className="container mx-auto text-center">
-                <h2 className="text-3xl font-bold mb-12">{data? data.title: "Pricing"}</h2>
+                {data.map((item, index) => {
+                    if (index === 0) {
+                        return (
+                            <Render
+                                key={index}
+                                data={item}
+                                content={content[index]}></Render>
+                        )
+                    }
+
+            })}
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+                    {data.map((item, index) => {
+                        if (index > 0 && index < data.length) {
+                            return (
+                                <div
+                                    key={index}
+                                    className="border border-gray-300 rounded-lg p-8 shadow-md">
+                                    <Render
+                                        data={item}
+                                        content={content[index]}></Render>
+                                </div>
+                            )
+                        }
+                    })}
+                </div>
+
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
                     <div className="border border-gray-300 rounded-lg p-8 shadow-md">
-                        {/*<h3 className="text-xl font-semibold mb-4">{data.categories[0].name}</h3>*/}
-                        {/*<p className="text-4xl font-bold mb-6">{data.categories[0].price}</p>*/}
+                        <h3 className="text-xl font-semibold mb-4">Starter</h3>
+                        <p className="text-4xl font-bold mb-6">$3.99</p>
                         <ul className="text-gray-700 mb-8">
-                            {/*<li className="mb-2">{data.categories[0].features[0]}</li>*/}
-                            {/*<li className="mb-2">{data.categories[0].features[1]}</li>*/}
-                            {/*<li>{data.categories[0].features[2]}</li>*/}
+                            <li className="mb-2">feature 1</li>
+                            <li className="mb-2">feature 2</li>
+                            <li>feature 3</li>
                         </ul>
                         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                             Get Started
                         </button>
                     </div>
                     <div className="border border-gray-300 rounded-lg p-8 shadow-md">
-                        {/*<h3 className="text-xl font-semibold mb-4">{data.categories[1].name}</h3>*/}
-                        {/*<p className="text-4xl font-bold mb-6">{data.categories[1].price}</p>*/}
+                        <h3 className="text-xl font-semibold mb-4">Plus</h3>
+                        <p className="text-4xl font-bold mb-6">9.99</p>
                         <ul className="text-gray-700 mb-8">
-                            {/*<li className="mb-2">{data.categories[1].features[0]}</li>*/}
-                            {/*<li className="mb-2">{data.categories[1].features[1]}</li>*/}
-                            {/*<li>{data.categories[1].features[2]}</li>*/}
+                            <li className="mb-2">feature</li>
+                            <li className="mb-2">feature</li>
+                            <li>feature</li>
                         </ul>
                         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                             Get Started
                         </button>
                     </div>
                     <div className="border border-gray-300 rounded-lg p-8 shadow-md">
-                        {/*<h3 className="text-xl font-semibold mb-4">{data.categories[2].name}</h3>*/}
-                        {/*<p className="text-4xl font-bold mb-6">{data.categories[2].price}</p>*/}
+                        <h3 className="text-xl font-semibold mb-4">Pro</h3>
+                        <p className="text-4xl font-bold mb-6">12.99</p>
                         <ul className="text-gray-700 mb-8">
-                            {/*<li className="mb-2">{data.categories[2].features[0]}</li>*/}
-                            {/*<li className="mb-2">{data.categories[2].features[1]}</li>*/}
-                            {/*<li>{data.categories[2].features[2]}</li>*/}
+                            <li className="mb-2">feature</li>
+                            <li className="mb-2">feature</li>
+                            <li>feature</li>
                         </ul>
                         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                             Contact Sales
