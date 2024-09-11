cbaee31ebd0f9f52e1a34736a7b4db3312fc097b
kwasinsiah <truegrayowl@gmail.com>
2024-09-11 12:35:36 -0400

    Renamed template1 and moved layout from studio

---
+++ src/app/templates/template1.js
@@ -0,0 +1,563 @@
+export const template1 ={
+  "layout": [
+    {
+      "id": 2,
+      "type": 'hero',
+      elements: [
+        {
+          id: 1001,
+          type: "text",
+          textType: "heading",
+          fontSize: "48px",
+          fontWeight: "bold",
+          color: "#FFFFFF",
+          marginBottom: "20px"
+        },
+        {
+          id: 1002,
+          type: "text",
+          textType: "subheading",
+          fontSize: "48px",
+          fontWeight: "bold",
+          color: "#FFFFFF",
+          marginBottom: "20px"
+        },
+        {
+          id: 1003,
+          type: "text",
+          textType: "body",
+          fontSize: "24px",
+          fontWeight: "normal",
+          color: "#FFFFFF",
+          marginBottom: "40px"
+        },
+        {
+          id: 1004,
+          type: "button",
+          textType: "button",
+          textColor: "#FFFFFF",
+          backgroundColor: "#FF5733",
+          fontSize: "18px",
+          padding: "10px 30px",
+          borderRadius: "5px"
+        },
+      ]
+    },
+    {id: 3, type: 'benefits', elements: [
+      {
+        type: "text",
+        textType: "subheading",
+        styles: {
+          color: "#000000",
+          marginBottom: "30px"
+        }
+      },
+      {
+        id: 101,
+        type: "container",
+        orientation: "horizontal",
+        elements: [
+          {
+            id: 201,
+            type: "container",
+            orientation: "vertical",
+            elements: [
+              {
+                type: "text",
+                textType: "body",
+                styles: {
+                  color: "#000000",
+                  // marginBottom: "30px"
+                }
+              },
+              {
+                type: "text",
+                textType: "body",
+                styles: {
+                  color: "#FF0000",
+                  // marginBottom: "30px"
+                }
+              },
+              {
+                type: "text",
+                textType: "body",
+                styles: {
+                  color: "#FF0000",
+                  // marginBottom: "30px"
+                }
+              },
+              {
+                type: "text",
+                textType: "body",
+                styles: {
+                  color: "#FF0000",
+                  marginBottom: "30px"
+                }
+              },
+            ]
+          },
+          {
+            id: 201,
+            type: "container",
+            orientation: "vertical",
+            elements: [
+              {
+                type: "text",
+                textType: "body",
+                styles: {
+                  color: "#FF0000",
+                  // marginBottom: "30px"
+                }
+              },
+              {
+                type: "text",
+                textType: "body",
+                styles: {
+                  color: "#FF0000",
+                  // marginBottom: "30px"
+                }
+              },
+              {
+                type: "text",
+                textType: "body",
+                styles: {
+                  color: "#FF0000",
+                  // marginBottom: "30px"
+                }
+              },
+              {
+                type: "text",
+                textType: "body",
+                styles: {
+                  color: "#FF0000",
+                  // marginBottom: "30px"
+                }
+              },
+            ]
+          },
+          {
+            id: 201,
+            type: "container",
+            orientation: "vertical",
+            elements: [
+              {
+                type: "text",
+                textType: "body",
+                styles: {
+                  color: "#FF0000",
+                  // marginBottom: "30px"
+                }
+              },
+              {
+                type: "text",
+                textType: "body",
+                styles: {
+                  color: "#FF0000",
+                  // marginBottom: "30px"
+                }
+              },
+              {
+                type: "text",
+                textType: "body",
+                styles: {
+                  color: "#FF0000",
+                  // marginBottom: "30px"
+                }
+              },
+              {
+                type: "text",
+                textType: "body",
+                styles: {
+                  color: "#FF0000",
+                  // marginBottom: "30px"
+                }
+              },
+            ]
+          },
+        ]
+      },
+
+    ]},
+    {id: 4, type: 'features', elements: [
+      {
+        type: "text",
+        textType: "subheading",
+        styles: {
+          color: "#000000",
+          marginBottom: "30px"
+        }
+      },
+      {
+        id: 101,
+        type: "container",
+        orientation: "horizontal",
+        elements: [
+          {
+            id: 201,
+            type: "container",
+            orientation: "vertical",
+            elements: [
+              {
+                type: "text",
+                textType: "body",
+                styles: {
+                  color: "#000000",
+                }
+              },
+              {
+                type: "text",
+                textType: "body",
+                styles: {
+                  color: "#FF0000",
+                }
+              },
+              {
+                type: "text",
+                textType: "body",
+                styles: {
+                  color: "#FF0000",
+                }
+              },
+              {
+                type: "text",
+                textType: "body",
+                styles: {
+                  color: "#FF0000",
+                  marginBottom: "30px"
+                }
+              },
+            ]
+          },
+          {
+            id: 201,
+            type: "container",
+            orientation: "vertical",
+            elements: [
+              {
+                type: "text",
+                textType: "body",
+                styles: {
+                  color: "#FF0000",
+                }
+              },
+              {
+                type: "text",
+                textType: "body",
+                styles: {
+                  color: "#FF0000",
+                }
+              },
+              {
+                type: "text",
+                textType: "body",
+                styles: {
+                  color: "#FF0000",
+                }
+              },
+              {
+                type: "text",
+                textType: "body",
+                styles: {
+                  color: "#FF0000",
+                }
+              },
+            ]
+          },
+          {
+            id: 201,
+            type: "container",
+            orientation: "vertical",
+            elements: [
+              {
+                type: "text",
+                textType: "body",
+                styles: {
+                  color: "#FF0000",
+                }
+              },
+              {
+                type: "text",
+                textType: "body",
+                styles: {
+                  color: "#FF0000",
+                }
+              },
+              {
+                type: "text",
+                textType: "body",
+                styles: {
+                  color: "#FF0000",
+                }
+              },
+              {
+                type: "text",
+                textType: "body",
+                styles: {
+                  color: "#FF0000",
+                }
+              },
+            ]
+          },
+        ]
+      },
+    ]},
+    {id: 5, type: 'call-to-action', elements: []},
+    {id: 6, type: 'pricing', elements: [
+      {
+        type: "text",
+        textType: "subheading",
+        styles: {
+          color: "#000000",
+          marginBottom: "30px"
+        }
+      },
+      {
+        id: 201,
+        type: "container",
+        orientation: "vertical",
+        elements: [
+          {
+            type: "text",
+            textType: "body",
+            styles: {
+              color: "#FF0000",
+              // marginBottom: "30px"
+            }
+          },
+          {
+            type: "text",
+            textType: "body",
+            styles: {
+              color: "#FF0000",
+              // marginBottom: "30px"
+            }
+          },
+          {
+            type: "text",
+            textType: "body",
+            styles: {
+              color: "#FF0000",
+              // marginBottom: "30px"
+            }
+          },
+          {
+            type: "text",
+            textType: "body",
+            styles: {
+              color: "#FF0000",
+              // marginBottom: "30px"
+            }
+          },
+          {
+            type: "text",
+            textType: "body",
+            styles: {
+              color: "#FF0000",
+              // marginBottom: "30px"
+            }
+          },
+        ]
+      },
+      {
+        id: 201,
+        type: "container",
+        orientation: "vertical",
+        elements: [
+          {
+            type: "text",
+            textType: "body",
+            styles: {
+              color: "#FF0000",
+              // marginBottom: "30px"
+            }
+          },
+          {
+            type: "text",
+            textType: "body",
+            styles: {
+              color: "#FF0000",
+              // marginBottom: "30px"
+            }
+          },
+          {
+            type: "text",
+            textType: "body",
+            styles: {
+              color: "#FF0000",
+              // marginBottom: "30px"
+            }
+          },
+          {
+            type: "text",
+            textType: "body",
+            styles: {
+              color: "#FF0000",
+              // marginBottom: "30px"
+            }
+          },
+          {
+            type: "text",
+            textType: "body",
+            styles: {
+              color: "#FF0000",
+              // marginBottom: "30px"
+            }
+          },
+        ]
+      },
+      {
+        id: 201,
+        type: "container",
+        orientation: "vertical",
+        elements: [
+          {
+            type: "text",
+            textType: "body",
+            styles: {
+              color: "#FF0000",
+              // marginBottom: "30px"
+            }
+          },
+          {
+            type: "text",
+            textType: "body",
+            styles: {
+              color: "#FF0000",
+              // marginBottom: "30px"
+            }
+          },
+          {
+            type: "text",
+            textType: "body",
+            styles: {
+              color: "#FF0000",
+              // marginBottom: "30px"
+            }
+          },
+          {
+            type: "text",
+            textType: "body",
+            styles: {
+              color: "#FF0000",
+              // marginBottom: "30px"
+            }
+          },
+          {
+            type: "text",
+            textType: "body",
+            styles: {
+              color: "#FF0000",
+              // marginBottom: "30px"
+            }
+          },
+        ]
+      },
+    ]},
+    {id: 7, type: 'testimonials', elements: [
+      {
+        type: "text",
+        textType: "subheading2",
+        styles: {
+          color: "#000000",
+          marginBottom: "30px"
+        }
+      },
+      {
+        id: 201,
+        type: "container",
+        orientation: "vertical",
+        elements: [
+          {
+            type: "text",
+            textType: "body",
+            styles: {
+              color: "#000000",
+              marginBottom: "2px"
+            }
+          },
+          {
+            type: "text",
+            textType: "body",
+            styles: {
+              color: "#000000",
+              marginBottom: "0px",
+              fontWeight: "bold",
+            }
+          },
+          {
+            type: "text",
+            textType: "body",
+            styles: {
+              color: "#000000",
+            }
+          },
+        ]
+      },
+      {
+        id: 201,
+        type: "container",
+        orientation: "vertical",
+        elements: [
+          {
+            type: "text",
+            textType: "body",
+            styles: {
+              color: "#000000",
+              marginBottom: "2px"
+            }
+          },
+          {
+            type: "text",
+            textType: "body",
+            styles: {
+              color: "#000000",
+              marginBottom: "0px",
+              fontWeight: "bold",
+            }
+          },
+          {
+            type: "text",
+            textType: "body",
+            styles: {
+              color: "#000000",
+            }
+          },
+        ]
+      },
+      {
+        id: 201,
+        type: "container",
+        orientation: "vertical",
+        elements: [
+          {
+            type: "text",
+            textType: "body",
+            styles: {
+              color: "#000000",
+              marginBottom: "2px"
+            }
+          },
+          {
+            type: "text",
+            textType: "body",
+            styles: {
+              color: "#000000",
+              marginBottom: "0px",
+              fontWeight: "bold",
+            }
+          },
+          {
+            type: "text",
+            textType: "body",
+            styles: {
+              color: "#000000",
+            }
+          },
+        ]
+      }
+    ]},
+    // {id: 8, type: 'faq', elements: []},
+    {id: 9, type: 'footer', elements: []},
+  ],
+  "content": [],
+}
