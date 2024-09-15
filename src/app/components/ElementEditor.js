1be36a3173b1a87bc3f27d97fab196700ebceb9f
kwasinsiah <truegrayowl@gmail.com>
2024-09-14 22:49:27 -0400

    Added more fields for Text modification

--- src/app/components/ElementEditor.js
+++ src/app/components/ElementEditor.js
@@ -1,20 +1,13 @@
 import {Input} from "@/components/ui/input";
 import {Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
 
-export const ElementEditor = ({data, content, activeElement, toggleActive, findElement, elementType}) => {
-    console.log("ElementEditor, activeElement", activeElement)
-    console.log(elementType)
+export const ElementEditor = ({data, content, activeElement, toggleActive, findElement, elementType, updateContent, updateElement}) => {
+    console.log("@@ ElementEditor, activeElement", activeElement)
+    console.log("@@ ElementEditor, elementType",elementType)
     let currentElement = null;
 
     const displayElementEditor = () => {
-        console.log(activeElement)
-        if (activeElement) {
-
-            currentElement = findElement(activeElement)
-            console.log(currentElement)
-        }
-
-        switch (currentElement?.element.type) {
+        switch (activeElement?.layout?.element.type) {
             case 'text':
                 return textEditor()
             default:
@@ -22,12 +15,45 @@ export const ElementEditor = ({data, content, activeElement, toggleActive, findE
         }
     }
 
+    const handleContentChange = (event) => {
+        console.log("@@ handleContentChange", event.target.value)
+        const newContent = activeElement?.content
+        newContent.element.content = event.target.value
+        console.log("@@ activeElement content", activeElement.content)
+        const newElement = {...activeElement, content: newContent}
+        console.log("@@ newElement", newElement)
+        updateContent(activeElement.id, activeElement.content.sectionId , newContent)
+    }
+
+
     const textEditor = () => {
-
-
-
         return (
             <div>
+                <div>
+                    <h4>Text</h4>
+                    <Input
+                        type={`text`}
+                        placeholder={`${activeElement?.content.element.content}`}
+                        value={`${activeElement?.content.element.content}`}
+                        onChange={handleContentChange}
+                    />
+                    <h4>Padding</h4>
+                    <Input/>
+                    <h4>Margin</h4>
+                    <Input/>
+                </div>
+                <div>
+                    <h4>Font</h4>
+                    <Input/>
+                    <h4>Size</h4>
+                    <Input/>
+                    <h4>Weight</h4>
+                    <Input/>
+                    <h4>Line spacing</h4>
+                    <Input/>
+                    <h4>Letter spacing</h4>
+                    <Input/>
+                </div>
                 <h4>Type</h4>
                 <Select>
                     <SelectTrigger className={`w-[180px]`}>
